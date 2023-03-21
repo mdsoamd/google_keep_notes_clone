792189669aa4cb1078dad50ef1bff15f53107ef2
Md Somad <mdsomad10@gmail.com>
2023-03-21 20:25:54 +0530

    Firebase_SetUp_Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_keep_notes_clone/firebase_options.dart';
 import 'package:google_keep_notes_clone/home.dart';
 
-void main() {
+void main() async {
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
+
+
+
+
+
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
